fe915901c96a0c598e5c3ebef2ca8932ef663cf8
Stuart <gravatar@strudel.org.uk>
2018-09-24 12:12:57 +0100

    Update Javascript to format the examples in a page

--- resources/formatexamples.js
+++ resources/formatexamples.js
@@ -1,12 +1,12 @@
 S(document).ready(function(){
 
 	// Format examples nicely to show how they were done
-	ex = S('.example');
+	var examples = S('.example');
 	function tidy(t){ return t.replace(/===NEWLINE===/g,"\n").replace(/\n*$/,"").replace(/^\n*/,""); }
 	function sanitise(t){ return t.replace(/\</g,"&lt;").replace(/\>/g,"&gt;"); }
 	function deindent(t){ var indent = ""; t.replace(/^([\s\t]+)/,function(m,p1,p2){ indent = m; }); return t.replace(new RegExp(indent,'g'),"").replace(/[\s\t]+$/,""); }
-	for(var i = 0; i < ex.length; i++){
-		html = ex[i].innerHTML;
+	for(var i = 0; i < examples.length; i++){
+		html = examples[i].innerHTML;
 		css = "";
 		js = "";
 		temp = html.replace(/\n/g,"===NEWLINE===").replace(/<!-- HTML -->/i,"");
@@ -23,10 +23,10 @@ S(document).ready(function(){
 		code = code.replace(/(src|href)=\"([^\"]+)\"/g,function(m,p1,p2){ return p1+'="<a href="'+p2+'">'+p2+'</a>"'; });
 		
 		showtitle = true;
-		if(S(ex[i]).attr('data-title')=="false") showtitle = false;
+		if(S(examples[i]).attr('data-title')=="false") showtitle = false;
 
 		// Append the 'How to do it' content
-		S(ex[i]).append((showtitle ? '<h3>How to do it</h3><h4>HTML</h4>':'')+'<pre class="prettyprint lang-html">'+deindent(code)+'</pre>'+(css ? (showtitle ? '<h4>CSS</h4>':'')+'<pre class="prettyprint lang-css">'+deindent(sanitise(css))+'</pre>':'')+(js ? (showtitle ? '<h4>Javascript</h4>':'')+'<pre class="prettyprint lang-js">'+deindent(sanitise(js))+'</pre>':''))
+		S(examples[i]).append((showtitle ? '<h3>How to do it</h3><h4>HTML</h4>':'')+'<pre class="prettyprint lang-html">'+deindent(code)+'</pre>'+(css ? (showtitle ? '<h4>CSS</h4>':'')+'<pre class="prettyprint lang-css">'+deindent(sanitise(css))+'</pre>':'')+(js ? (showtitle ? '<h4>Javascript</h4>':'')+'<pre class="prettyprint lang-js">'+deindent(sanitise(js))+'</pre>':''))
 	}
 
 	// Update anchor now that we've updated the page		
