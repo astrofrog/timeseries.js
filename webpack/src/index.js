c48fce44af5586eb888ff165398da3b32ba639af
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-02-04 21:47:14 +0000

    Simplify webpack configuration to just produce single js file

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,19 +2,7 @@ self.Big = require('../../resources/big.min.js');
 require('../../resources/stuquery.js');
 require('../../resources/graph.js');
 require('../../resources/timeseries.js');
+
+require('../../resources/aas.css');
+require('../../resources/style.css');
 require('../../resources/timeseries.css');
-
-function component() {
-
-  let element = document.createElement('div');
-  element.setAttribute("id", "example1");
-  return element;
-
-}
-
-document.body.appendChild(component());
-
-S(document).ready(function(){
-  ex1 = TimeSeries.create("lightcurve.json");
-  ex1.initialize(document.getElementById('example1'));
-});
